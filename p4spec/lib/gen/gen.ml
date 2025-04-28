54022a11dd79b4b71edd58f895ada521a671621d
KunJeong <kunjeong99@naver.com>
2025-04-28 14:52:59 +0900

    refactor: replaced enumeration algorithm with random-select-one

--- p4spec/lib/gen/gen.ml
+++ p4spec/lib/gen/gen.ml
@@ -14,49 +14,33 @@ let load_def (tdenv: TDEnv.t) (def : def) : TDEnv.t =
 let load_spec (tdenv: TDEnv.t) (spec : spec) : TDEnv.t =
   List.fold_left load_def tdenv spec
 
+let random_select (values : value list) : value =
+  Random.int (List.length values) |> List.nth values
+
 (* Take a SpecTec syntax and expand it into values*)
-let rec expand_typ ?(depth=0) (tdenv : TDEnv.t) (typ : typ): value list =
+let rec expand_typ ?(depth=0) (tdenv : TDEnv.t) (typ : typ): value =
   depth |> ignore;
   expand_typ' tdenv typ.it
 
-(* Takes a list of N types and returns a list of all possible expansion combinations,
- * where each combination is a list of N values;
- *
- * If expand_typ(t_i) = [ v_(i,1); ...; v_(i,l_i) ] then
- * Result is [ [ v_(1,1); ... ; v_(N,1) ]; ... [ v_(1,l_1); ... ; v_(N;l_N) ] *)
-and expand_typs tdenv typs : value list list =
-  List.fold_left
-    (fun (values_acc: value list list) typ -> 
-       let values_t = expand_typ tdenv typ in
-       List.concat_map 
-         (fun (value_acc: value list) ->
-            List.map 
-              (fun (value_t: value) -> value_acc @ [ value_t ]) values_t)
-         values_acc
-    )
-    [] typs
+and expand_typs tdenv typs : value list =
+  List.map (expand_typ tdenv) typs 
 
-and expand_typ' tdenv typ': value list =
+and expand_typ' tdenv typ': value =
   match typ' with
-  | BoolT -> [ BoolV true; BoolV false ]
+  | BoolT -> [ BoolV true; BoolV false ] |> random_select
   | NumT `NatT ->
-    [ NumV (`Nat (Bigint.of_int 0)); NumV (`Nat (Bigint.of_int 4)); NumV (`Nat (Bigint.of_int 6)) ] 
+    [ NumV (`Nat (Bigint.of_int 0)); NumV (`Nat (Bigint.of_int 4)); NumV (`Nat (Bigint.of_int 6)) ] |> random_select
   | NumT `IntT -> 
-    [ NumV (`Int (Bigint.of_int 2)); NumV (`Int (Bigint.of_int (-2))) ] 
-  | TextT -> [ TextV "a"; TextV "b" ]
+    [ NumV (`Int (Bigint.of_int 2)); NumV (`Int (Bigint.of_int (-2))) ] |> random_select
+  | TextT -> [ TextV "a"; TextV "b" ] |> random_select
   | TupleT typs_inner ->
-    let values_combinations = expand_typs tdenv typs_inner in
-    List.map (fun values -> TupleV values) values_combinations
+    TupleV (expand_typs tdenv typs_inner)
   | IterT (typ_inner, Opt) -> 
-    (* Either none or list of value candidates *)
-    (* TODO: generate random list of length <= n*)
-    OptV (Option.None) ::
-    List.map (fun v -> OptV (Option.some v)) (expand_typ tdenv typ_inner)
+    [ OptV (Option.None); OptV (Option.some (expand_typ tdenv typ_inner)) ] |> random_select
   | IterT (typ_inner, List) ->
-    [
-      ListV ((expand_typ tdenv typ_inner))
-    ]
-  | FuncT -> [ ] 
+    let l = Random.int 3 in
+      ListV (List.init l (fun _ -> expand_typ tdenv typ_inner))
+  | FuncT -> FuncV ("f" $ no_region) 
   | VarT (tid, targs) -> 
     let td  = TDEnv.find_opt tid tdenv in      
     match td with
@@ -68,12 +52,10 @@ and expand_typ' tdenv typ': value list =
           expand_typ tdenv typ 
         | StructT typfields ->
           let atoms, typs = List.split typfields in
-          let values_combinations  =
+          let values  =
             subst_typs theta typs |> expand_typs tdenv
           in
-          List.map (fun values ->
-              StructV (List.combine atoms values)
-            ) values_combinations
+          StructV (List.combine atoms values)
         | VariantT typcases -> 
           let nottyps' = List.map it typcases in
           let nottyps' = 
@@ -84,16 +66,15 @@ and expand_typ' tdenv typ': value list =
           in
           let expand_nottyp' nottyp'= 
             let mixop, typs = nottyp' in
-            let values_combinations = expand_typs tdenv typs in
-            List.map (fun values -> CaseV (mixop, values))
-              values_combinations
+            let values = expand_typs tdenv typs in
+            CaseV (mixop, values)
           in
-          List.concat_map expand_nottyp' nottyps'
+          List.map expand_nottyp' nottyps' |> random_select
       )
-    | None -> []
+    | None -> failwith "not found in tdenv"
 
-
-let expand depth spec typ : value list = 
+let expand depth spec typ : value =
+  Random.init 161;
   let tdenv = TDEnv.empty in
   let tdenv = load_spec tdenv spec in
   expand_typ ~depth tdenv typ
