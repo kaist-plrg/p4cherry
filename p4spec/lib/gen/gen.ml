2fcd43ee3de30dd6b4f50737230c114c50d1c77e
KunJeong <kunjeong99@naver.com>
2025-04-28 15:50:41 +0900

    feat: changed generator signature to `value option` and added logic for forced termination

--- p4spec/lib/gen/gen.ml
+++ p4spec/lib/gen/gen.ml
@@ -14,18 +14,27 @@ let load_def (tdenv: TDEnv.t) (def : def) : TDEnv.t =
 let load_spec (tdenv: TDEnv.t) (spec : spec) : TDEnv.t =
   List.fold_left load_def tdenv spec
 
-let random_select (values : value list) : value =
-  Random.int (List.length values) |> List.nth values
+let random_select (values : value list) : value option =
+  Random.int (List.length values) |> List.nth values |> Option.some
+
+let ( let* ) x f = Option.bind x f
+
+let bind_list (opt_list : value option list) : value list option =
+    if opt_list |> List.exists (Option.is_none) then None
+    else opt_list |> List.map (Option.get) |> Option.some
 
 (* Take a SpecTec syntax and expand it into values*)
-let rec expand_typ ?(depth=0) (tdenv : TDEnv.t) (typ : typ): value =
-  depth |> ignore;
-  expand_typ' tdenv typ.it
+let rec expand_typ depth (tdenv : TDEnv.t) (typ : typ): value option =
+  if depth <= 0 then None
+  else
+  expand_typ' depth tdenv typ.it
 
-and expand_typs tdenv typs : value list =
-  List.map (expand_typ tdenv) typs 
+and expand_typs depth tdenv typs : value list option=
+  if depth <= 0 then None
+  else List.map (expand_typ depth tdenv) typs |> bind_list
 
-and expand_typ' tdenv typ': value =
+and expand_typ' depth tdenv typ': value option =
+  let depth = depth-1 in
   match typ' with
   | BoolT -> [ BoolV true; BoolV false ] |> random_select
   | NumT `NatT ->
@@ -34,13 +43,19 @@ and expand_typ' tdenv typ': value =
     [ NumV (`Int (Bigint.of_int 2)); NumV (`Int (Bigint.of_int (-2))) ] |> random_select
   | TextT -> [ TextV "a"; TextV "b" ] |> random_select
   | TupleT typs_inner ->
-    TupleV (expand_typs tdenv typs_inner)
+    let* values_inner = expand_typs depth tdenv typs_inner in
+    TupleV values_inner |> Option.some
   | IterT (typ_inner, Opt) -> 
-    [ OptV (Option.None); OptV (Option.some (expand_typ tdenv typ_inner)) ] |> random_select
+    let i = Random.int 2 in
+    if i = 0 then OptV (None) |> Option.some
+    else
+      (let* value_inner = expand_typ depth tdenv typ_inner in
+      OptV (Some value_inner) |> Option.some)
   | IterT (typ_inner, List) ->
     let l = Random.int 3 in
-      ListV (List.init l (fun _ -> expand_typ tdenv typ_inner))
-  | FuncT -> FuncV ("f" $ no_region) 
+    let* values_inner = List.init l (fun _ -> expand_typ depth tdenv typ_inner) |> bind_list in
+    ListV (values_inner) |> Option.some
+  | FuncT -> None
   | VarT (tid, targs) -> 
     let td  = TDEnv.find_opt tid tdenv in      
     match td with
@@ -49,13 +64,13 @@ and expand_typ' tdenv typ': value =
         match typdef.it with
         | PlainT typ ->
           let typ = subst_typ theta typ in
-          expand_typ tdenv typ 
+          expand_typ depth tdenv typ 
         | StructT typfields ->
           let atoms, typs = List.split typfields in
-          let values  =
-            subst_typs theta typs |> expand_typs tdenv
+          let* values =
+            typs |> subst_typs theta |> expand_typs depth tdenv
           in
-          StructV (List.combine atoms values)
+          StructV (List.combine atoms values) |> Option.some
         | VariantT typcases -> 
           let nottyps' = List.map it typcases in
           let nottyps' = 
@@ -66,15 +81,16 @@ and expand_typ' tdenv typ': value =
           in
           let expand_nottyp' nottyp'= 
             let mixop, typs = nottyp' in
-            let values = expand_typs tdenv typs in
-            CaseV (mixop, values)
+            let* values = expand_typs depth tdenv typs in
+            CaseV (mixop, values) |> Option.some
           in
-          List.map expand_nottyp' nottyps' |> random_select
+          (* randomly selects from only successful CaseVs *)
+          List.map expand_nottyp' nottyps' |> List.filter (Option.is_some) |> List.map (Option.get) |> random_select
       )
-    | None -> failwith "not found in tdenv"
+    | None -> None
 
-let expand depth spec typ : value =
+let expand depth spec typ : value option =
   Random.init 161;
   let tdenv = TDEnv.empty in
   let tdenv = load_spec tdenv spec in
-  expand_typ ~depth tdenv typ
+  expand_typ depth tdenv typ
